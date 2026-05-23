76f2326fd20972eadf88ca262761bfda61261b9f
zun <dunggg1359@gmail.com>
2026-05-23 22:59:29 +0700

    add frame project

---
+++ TravelVista/src/main/java/com/qd/service/impl/CategotyServiceImpl.java
@@ -0,0 +1,28 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package com.qd.service.impl;
+//
+//import com.qd.pojo.Category;
+//import com.qd.repository.CategoryRepository;
+//import com.qd.service.CategoryService;
+//import java.util.List;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+///**
+// *
+// * @author ADMIN
+// */
+//@Service
+//public class CategotyServiceImpl implements CategoryService{
+//    @Autowired
+//    private CategoryRepository cateRepo;
+//    
+//    @Override
+//    public List<Category> getCates() {
+//        return this.cateRepo.getCates();
+//    }
+//    
+//}
