6890bcbe0a197c6bf77b35d016e052d5c6802f02
zun <dunggg1359@gmail.com>
2026-05-23 23:51:16 +0700

    update WebSecurityConfig

--- TravelVista/src/main/java/com/qd/configs/WebSecurityConfig.java
+++ TravelVista/src/main/java/com/qd/configs/WebSecurityConfig.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.qd.configs;
 
-/**
- *
- * @author ADMIN
- */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            //Tắt CSRF để các công cụ test API (Swagger, Postman) gửi request thoải mái
+            .csrf(csrf -> csrf.disable())
+            
+            // Cấu hình luật phân quyền mở
+            .authorizeHttpRequests(auth -> auth
+            // MỞ TOÀN BỘ: Cho phép truy cập bất kỳ URL nào mà không cần đăng nhập hay Token
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
