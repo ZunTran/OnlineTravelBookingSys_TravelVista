499d2316ccf7f70b2ed8faf95fde84e7f4456f17
Han <hanhxuk@gmail.com>
2026-05-24 01:11:16 +0700

    create frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <p className="flex text-red-500">
+            Home
+        </p>
+    );
+}
+
+export default Header;
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+const LoginPage = () => {
+    return (
+        <h1 className="text-3xl">Login</h1>
+    );
+
+}
+
+export default LoginPage;
